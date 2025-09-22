f810a5df23e97519b902a5011e15451283bc9a78
iamdnikolaev <160739477+iamdnikolaev@users.noreply.github.com>
2025-09-22 09:11:18 +0300

    Этап 1. Упрощенный вариант со stats-server на порту 9090. Иначе без запущенного stats-server не проинициализируется переменная statClient в коде main-service. А на гитхабе именно запуск main-service проверяется сразу после config-server.

--- stats/stats-client/src/main/java/ru/practicum/StatClient.java
+++ stats/stats-client/src/main/java/ru/practicum/StatClient.java
@@ -1,13 +1,8 @@
 package ru.practicum;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
-import org.springframework.retry.backoff.FixedBackOffPolicy;
-import org.springframework.retry.policy.MaxAttemptsRetryPolicy;
-import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -23,31 +18,12 @@ import java.util.List;
 @Slf4j
 public class StatClient {
     private final RestClient restClient;
-    private final DiscoveryClient discoveryClient;
-
-    /*@Value("${discovery.services.stats-server-id}")
-    private String statsServerId;*/
 
     /**
      * Instantiates a new Stat client.
      */
-    public StatClient(DiscoveryClient discoveryClient) {
-        this.discoveryClient = discoveryClient;
-        RetryTemplate retryTemplate = new RetryTemplate();
-
-        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
-        fixedBackOffPolicy.setBackOffPeriod(3000L);
-        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
-
-        MaxAttemptsRetryPolicy retryPolicy = new MaxAttemptsRetryPolicy();
-        retryPolicy.setMaxAttempts(3);
-        retryTemplate.setRetryPolicy(retryPolicy);
-
-        ServiceInstance statInstance = retryTemplate.execute(cxt -> getStatInstance());
-        String statBaseUrl = statInstance.getUri().toString();
-        this.restClient = RestClient.builder()
-                .baseUrl(statBaseUrl)
-                .build();
+    public StatClient(String clientUrl) {
+        this.restClient = RestClient.builder().baseUrl(clientUrl).build();
     }
 
     /**
@@ -115,17 +91,5 @@ public class StatClient {
             return "Ошибка чтения статистики посещений: ";
         } else
             return "Ошибка статистики посещений: ";
-    }
-
-    private ServiceInstance getStatInstance() {
-        String statsServerId = "stats-server";
-
-        try {
-            return discoveryClient
-                    .getInstances(statsServerId)
-                    .getFirst();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
